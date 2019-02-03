6f4ceae69f985ba1999e3940ba28f393e361d9c1
Lizzie Chan <lchan217@gmail.com>
2019-02-03 21:27:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.find_by_name(song_name)
     @@all.find {|name| name.name == song_name}
   end 
-  def self.find_or_create_by_name
-    
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
   end 
 end
